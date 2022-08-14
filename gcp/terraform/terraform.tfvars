664c54cd91e3152e83a869dfc6510219e3bcf200
Jay Lavine <jaylavine@gmail.com>
2022-08-14 12:24:15 -0700

    + gcp and terraform files

---
+++ gcp/terraform/terraform.tfvars
@@ -0,0 +1,2 @@
+project                  = "k8s-hardway-new"
+credentials_file         = "k8s-hardway-new-4eda0ce1a5b0.json"
---
+++ gcp/terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "project" { }
+
+variable "credentials_file" { }
+
+variable "region" {
+  default = "us-west1"
+}
+
+variable "zone" {
+  default = "us-west1-c"
+}
